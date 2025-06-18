044926ab9e8e1314ba4eb06cc169152a108f1666
Luis Eduardo Sagnay Pilamunga <sagnayluis99@gmail.com>
2025-06-18 09:33:31 -0500

    ejercicios

---
+++ notasAngular.ts
@@ -0,0 +1,21 @@
+/**
+ * Notas sobre angular
+ * - Angular es un framework de desarrollo web de código abierto mantenido por Google.
+ * - Utiliza TypeScript como su lenguaje principal, lo que permite una mejor tipificación y
+ *  autocompletado en los editores de código.
+ *  - Se maneja con modulos, componentes y servicios.
+ * - Los componentes son la base de las aplicaciones Angular y encapsulan la lógica, el estilo y la
+ * vista de una parte de la interfaz de usuario.
+ * - Los modulos son contenedores que agrupan componentes, directivas y servicios relacionados.
+ * - Los servicios son clases que proporcionan funcionalidades reutilizables y se inyectan en
+ * componentes y otros servicios a través del sistema de inyección de dependencias.
+ * - Nuevo modelo de recreatividad mas eficiente 
+ *      const nombre = signal("nombre")
+ *      nombre.set("nuevo nombre")
+ *      nombre() // devuelve el valor actual
+ * - Otras partes importantes
+ *       HttpClientModule para consumir APIs REST
+ *       FormsModule para manejar formularios
+ *       Pipes para transformar datos en las vistas
+ *       
+ */
